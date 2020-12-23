f7cb294b581c44505d424eb5347fcbae5c43b4a6
MoisesCava <moiseslsth2204@gmail.com>
2020-12-23 15:25:16 -0400

    Clases con Hooks hasta le modulo 4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,124 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import person from './Person/Person';
 import Person from './Person/Person'
+import UserInput from './UserInput/UserInput'
+import UserOutput from './UserOutput/UserOutput'
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       <h1>Hola, soy la primera app en react</h1>
-       <p>Realmente funciona</p>
-       <Person name="Moises" age="24"/>
-       <Person name="Maria" age="23"> Su hobby es dormir </Person>
-       <Person name="Gabriela" age="23"/>
-      </div>
-    );
+const app = props => {
+
+  const [showState, setShowState] = useState(false)
+
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      {
+        name: 'Moises',
+        age: 24
+      },
+      {
+        name: 'Maria',
+        age: 23
+      },
+      {
+        name: 'Gabriela',
+        age: 23
+      }
+    ]
+  });
+
+  const [otroEstado, setOtroEstado] = useState('algun otro valor');
+
+  console.log(personsState, otroEstado);
+
+  const switchNameHandler = (newName) => {
+    setPersonsState({
+      persons: [
+        {
+          name: newName,
+          age: 24
+        },
+        {
+          name: 'Maria',
+          age: 23
+        },
+        {
+          name: 'Gabriela',
+          age: 25
+        }
+      ]
+    });
   }
+
+  const [inputState, setInputState] = useState('Camila')
+
+  const nameChangeHandler = (event) => {
+    setPersonsState({
+      persons: [
+        {
+          name: 'Reinel',
+          age: 24
+        },
+        {
+          name: event.target.value,
+          age: 23
+        },
+        {
+          name: 'Gabriela',
+          age: 25
+        }
+      ]
+    });
+  }
+
+  const inputChangeHandler = (event) => {
+    setInputState(
+      event.target.value)
+  }
+
+  const togglePersonHandler = () => {
+    setShowState(!showState)
+  }
+
+  let persons = null
+
+  if (showState) {
+    persons = (
+      <div >  
+        {personsState.persons.map(person => {
+          return <Person name={person.name} age={person.age}/>
+        })}
+        {/* <Person
+          name={personsState.persons[0].name}
+          age={personsState.persons[0].age}
+        />
+        <Person
+          click={switchNameHandler}
+          name={personsState.persons[1].name}
+          age={personsState.persons[1].age}
+          changed={nameChangeHandler}
+        >
+          My Hobbies: Racing
+      </Person>
+        <Person
+          name={personsState.persons[2].
+            name}
+          age={personsState.persons[2].age}
+        />
+        <UserInput change={inputChangeHandler} />
+        <UserOutput name={inputState} /> */}
+      </div>
+    )
+  }
+
+  return (
+    <div className="App">
+      <h1>Hola soy la primera app en React</h1>
+      <p>Si funciona</p>
+      <button onClick={togglePersonHandler}>cambiar nombre</button>
+      {persons}
+    </div>
+  );
 }
 
-export default App;
+export default app;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import './Person.css'
+
 const person = (props) => {
-    
+
     return (
-        <div>
-            <p>Soy {props.name} y tengo {props.age} de edad</p>
+        <div className="Person">
+            <p onClick={props.click}>Soy {props.name} y tengo {props.age} de edad</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} />
         </div>
     );
 }
